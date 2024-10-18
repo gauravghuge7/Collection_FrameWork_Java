d2dfcedc25a565e563fe7c66e5319e118d3ea673
Gaurav Ghuge <guduughuge7@gmail.com>
2024-10-18 11:12:16 +0530

    start the InterFace Detail in every Readme.md file

---
+++ LIst_Interface/Vector_In_Java/Introduction_to_Vector/index.java
@@ -0,0 +1,21 @@
+package Introduction_to_Vector;
+import java.util.Vector;
+
+class index {
+
+
+   public static void main(String[] args) {
+      
+      Vector<Integer> v = new Vector<>();
+
+      v.add(13);
+      v.add(13);
+      v.add(13);
+      v.add(13);
+      v.add(13);
+
+      
+      System.out.println(v);
+
+   }
+}
